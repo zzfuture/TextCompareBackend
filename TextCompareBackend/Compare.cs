3503f54f2266a01c46ed68ea92eb9c016add1dfc
Iker Acevedo <113058239+zzfuture@users.noreply.github.com>
2025-11-02 13:27:06 -0500

    main logic should be ok :D

---
+++ TextCompareBackend/Compare.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace TextCompareBackend;
+
+public static class CompareService
+{
+    public static bool GetTextDifference(string text1, string text2, out int[][] differenceIndexes)
+    {
+        differenceIndexes =  new int[2][]; // Posiciones que pueden estar diferentes en cada texto (un array)
+        differenceIndexes[0] = new int [text1.Length];
+        differenceIndexes[1] = new int [text2.Length];
+        
+        var equal = AreEqual(text1, text2 );
+        if (equal) return false; // No hay diferencias
+        
+        var sameLength = AreEqualLength(text1, text2);
+
+        if (sameLength)
+        {
+            GetIndexesFromEqualLengthText(text1, text2, out var text1Indexes, out var text2Indexes );
+            differenceIndexes[0] = text1Indexes;
+            differenceIndexes[1] = text2Indexes;
+        }
+        else
+        {
+            var maxLength =  Math.Max(text1.Length, text2.Length); // Calculamos cual es el maximo que podemos iterar
+        
+            GetShortAndLongText(text1, text2, out var shortText, out var longText);
+        
+            GetIndexesFromDifferentLengthText(
+                ref shortText, 
+                ref longText, 
+                ref maxLength, 
+                out var shortTextIndexes, 
+                out var longTextIndexes);
+            
+            differenceIndexes[0] = shortTextIndexes;
+            differenceIndexes[1] = longTextIndexes;
+        }
+        
+        
+
+        //differenceIndexes[1] = text2Indexes;
+
+        return true;
+    }
+
+    private static void GetIndexesFromEqualLengthText(
+        string text1, 
+        string text2,
+        out int[] text1Indexes,
+        out int[] text2Indexes)
+    {
+        text1Indexes =  new int[text1.Length]; // Array con index de caracteres con diferencia para text 1
+        text2Indexes =  new int[text2.Length]; // Array con index de caracteres con diferencia para text 2
+        
+        // Avanzamos con dos pointers, cuando una palabra no coincide avanzamos en ese texto
+        
+        for (var i = 0; i < text1.Length; i++)
+        {
+            if (text1[i].Equals(text2[i])) continue;
+
+            text1Indexes[i] = 1; // Marcamos la diferencia
+            text2Indexes[i] = 1; // Marcamos la diferencia
+        }
+    }
+    private static void GetIndexesFromDifferentLengthText(
+        ref string shortText, 
+        ref string longText, 
+        ref int maxLength,
+        out int[] shortTextIndexes,
+        out int[] longTextIndexes)
+    {
+        longTextIndexes =  new int[longText.Length];
+        shortTextIndexes =  new int[shortText.Length];
+        
+        for (var i = 0; i < maxLength; i++)
+        {
+            if (i > shortText.Length - 1)
+            {
+                FillEmptyIndexes(longTextIndexes, i);
+                break; // Cerramos el bucle porque no hay más que comparar
+            }
+
+            if (longText[i].Equals(shortText[i])) continue;
+
+            longTextIndexes[i] = 1; // Marcamos la diferencia
+            shortTextIndexes[i] = 1; // Marcamos la diferencia
+        }
+    }
+
+    private static bool AreEqualLength(string text1, string text2)
+    {
+        return text1.Length == text2.Length;
+    }
+    
+    private static void GetShortAndLongText(string text1, string text2, out string shortText, out string longText)
+    {
+        if (text1.Length >= text2.Length)
+        {
+            shortText = text2;
+            longText = text1;
+        }
+        else
+        {
+            shortText = text1;
+            longText = text2;
+        }
+    }
+    
+
+    private static int[] FillEmptyIndexes(int[] indexes, int indexStart)
+    {
+        for (var i = indexStart; i < indexes.Length; i++)
+        {
+            indexes[i] = 1;
+        }
+
+        return indexes;
+    }
+    
+    private static bool AreEqual(string text1, string text2)
+    {
+        return text1.Equals(text2);
+    }
+    private static bool AreEqual(char char1, char char2)
+    {
+        return char1.Equals(char2);
+    }
+}
---
+++ TextCompareBackend/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace TextCompareBackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var compare = CompareService.GetInstance();
+
+            string[] texts = { "Hello", "Hello" };
+            var text1 = texts[0];
+            var text2 = texts[1];
+            var difference = CompareService.GetTextDifference(text1, text2, out var differenceTexts);
+            
+            if (!difference) Console.WriteLine("Iguales :D");
+            else Console.WriteLine("Diferentes");
+            
+            // La impresion en consola puede que salga al reves, ya lo arreglo después :D
+            for(var i = 0; i < texts.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ", texts[i].ToCharArray()));
+                Console.WriteLine(string.Join(" ", differenceTexts[i]));
+            }
+            
+        }
+    }
+}
